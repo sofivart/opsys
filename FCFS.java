1d8c71df5af009d2f44f5b5de10e4e823138c431
linalys <37178369+linalys@users.noreply.github.com>
2020-01-12 15:20:43 +0200

    Update FCFS.java

--- FCFS.java
+++ FCFS.java
@@ -5,29 +5,28 @@ public class FCFS
     // Method that calculates the waiting time for all processes
     static int[] calcWaitingTime(int burstTime[], int quantum)
     {
-        int n = burstTime.length;
-        int [] time=new int[n];
-        time[0] = 0;
+        int n = burstTime.length;       //number of processes
+        int [] time=new int[n];     //an array with length n, which indicates when each process begins, that is the waiting time
+        time[0] = 0;        //process 0 begins at time 0
 
-        for(int i = 1; i<n; i++){
-            time[i]= burstTime[i-1] + time[i-1] ;
+        for(int i = 1; i<n; i++){       //for the rest of the processes
+            time[i]= burstTime[i-1] + time[i-1] ;       //the waiting time equals the burst and waiting times of the previous process
         }
-        for (int i=0; i<n; i++){
+        for (int i=0; i<n; i++){        //for every process, we reduce the waiting time by the index of the process, which is the arriving time of the process
             time[i]-=i;
         }
-        return time;
+        return time;        //returns array with waiting times
     }
 
     // Method that calculates turn around time for all processes
     static int[] calcTurnAroundTime(int burstTime[], int waitingTime[])
     {
-        int n = burstTime.length;
-        int [] time=new int[n];
-        int t = 0;
-        for(int i = 0; i<n; i++){
-            time[i]= burstTime[i] + waitingTime[i]  ;
+        int n = burstTime.length;       //number of processes
+        int [] time=new int[n];     //an array with length n, that contains the turnaround times
+        for(int i = 0; i<n; i++){       //loop for all processes
+            time[i]= burstTime[i] + waitingTime[i]  ;       //turnaround time equals the sum of burst and waiting times of the process
         }
-        return time;
+        return time;        //returns array with turnaround times
 
 
     }
