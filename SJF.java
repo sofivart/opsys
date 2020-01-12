81fbe6849215abe15cb0d5acde4ba6bb73575ed5
sofivart <37180608+sofivart@users.noreply.github.com>
2020-01-12 20:26:51 +0200

    Update SJF.java

--- SJF.java
+++ SJF.java
@@ -1,20 +1,22 @@
 public class SJF
 {
+    
     // Method that calculates the waiting time for all processes
     static int[] calcWaitingTime(int burstTime[], int quantum)
     {
         int n = burstTime.length;
-        int[] ar= new int[n];
+        int[] ar= new int[n]; //number of processes
         for(int i=0;i<n;i++){
-            ar[i]=i;
+            ar[i]=i; //ID of process
         }
-        int[] time=new int[n];
-        int[] bt;
-        bt = burstTime.clone();
+        int[] time=new int[n]; //the waiting time of each process
+        int[] bt; 
+        bt = burstTime.clone(); //copy the table burstTime[]->bt to make the appropriate changes
+        //sort the bt and ar according to the smallest burst time 
         for(int i=0;i<n-1;i++){
             for(int j=0;j<n-i-1;j++){
                 if(bt[j]>bt[j+1]){
-                    int temp=bt[j];
+                    int temp=bt[j]; //temp:helping role 
                     bt[j]=bt[j+1];
                     bt[j+1]=temp;
                     temp=ar[j];
@@ -23,15 +25,16 @@ public class SJF
                 }
             }
         }
+        //the waiting time is the previous waiting time plus the burst time of previoues process in the sorted table ar
         for(int i=1;i<n;i++){
             time[i]=time[i-1]+bt[i-1];
         }
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){//print IDs 
             System.out.println(ar[i]);
         }
-        int[]waitingtime=time.clone();
+        int[]waitingtime=time.clone(); //copy the table time to the table waitingtime
         for (int i=0;i<n;i++){
-            waitingtime[ar[i]]=time[i];
+            waitingtime[ar[i]]=time[i];// according to the classfication
         }
         return waitingtime;
 
@@ -40,13 +43,13 @@ public class SJF
     // Method that calculates turn around time for all processes
     static int[] calcTurnAroundTime(int burstTime[], int waitingTime[])
     {
-        int n = burstTime.length;
-        int[] ar= new int[n];
+        int n = burstTime.length; //number of processes
+        int[] ar= new int[n];//IDs
         for(int i=0;i<n;i++){
             ar[i]=i;
         }
-        int[] time=new int[n];
-
+        int[] time=new int[n]; 
+        //calculating turnaround time by adding burstTime and waitingTime for each process
         for(int i=0;i<n;i++){
             time[i]=burstTime[i]+waitingTime[i];
         }
@@ -77,8 +80,8 @@ public class SJF
         for (int i = 0; i < n; i++) {
             totalWaitingTime += waitingTime[i];
             totalTurnAroundTime += turnAroundTime[i];
-            System.out.println(i + "\t\t" + burstTime[i] +"\t " +
-                    waitingTime[i] +"\t\t " + turnAroundTime[i]);
+            System.out.println(i + "\t\t\t" + burstTime[i] +"\t\t\t " +
+                    waitingTime[i] +"\t\t\t " + turnAroundTime[i]);
         }
 
         System.out.println("\nAverage waiting time = " +
